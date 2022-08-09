b51062aa5d2128d05030c756e1a11e9fec6ca09f
hashim222 <aslamhashim19@gmail.com>
2022-08-09 19:27:16 +0000

    .....

--- products/models.py
+++ products/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 
 class Product(models.Model):
-    title = models.CharField(max_length=30)
-    description = models.TextField()
-    price = models.TimeField()
+    title = models.CharField(max_length=120)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(decimal_places=2, max_digits=10000)
+ 
