bc55f8ce6850eec1fe9b6c4e9a31197834a87705
hashim222 <aslamhashim19@gmail.com>
2022-08-14 12:40:35 +0000

    ...

--- products/forms.py
+++ products/forms.py
@@ -1,19 +1,48 @@
 from django import forms
 
+
 from .models import Product
 
 
 class ProductForm(forms.ModelForm):
+    title = forms.CharField(label='',
+                                  widget=forms.TextInput(attrs={"placeholder": "Your title"}))
+    description = forms.CharField(
+        required=False,
+        widget=forms.Textarea(
+            attrs={
+                "placeholder": "Your description",
+                "class": "new-class-name two",
+                "id": "my-id-for-textarea",
+                "rows": 20,
+                'cols': 120
+            }
+        )
+    )
+    price = forms.DecimalField(initial=199.99)
+
     class Meta:
         model = Product
-        fields = {
+        fields = [
             'title',
             'description',
             'price'
-        }
+        ]
 
-  
-class RowProductForm(forms.Form):
-    title = forms.CharField()
-    description = forms.CharField()
-    price = forms.DecimalField()
+
+class RawProductForm(forms.Form):
+    title = forms.CharField(label='', widget=forms.TextInput(
+        attrs={"placeholder": "Your title"}))
+    description = forms.CharField(
+        required=False,
+        widget=forms.Textarea(
+            attrs={
+                "placeholder": "Your description",
+                "class": "new-class-name two",
+                "id": "my-id-for-textarea",
+                "rows": 20,
+                'cols': 120
+            }
+        )
+    )
+    price = forms.DecimalField(initial=199.99)
